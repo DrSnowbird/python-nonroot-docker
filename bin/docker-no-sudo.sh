e6389de3b4ed60dddbaecaaffce047a7d1b6268a
DrSnowbird <DrSnowbird@openkbs.org>
2022-02-15 11:59:10 -0500

    updated/added: bin/docker-no-sudo.sh

--- bin/docker-no-sudo.sh
+++ bin/docker-no-sudo.sh
@@ -4,10 +4,8 @@ echo ">>> Add $USER to docker group to use docker command without sudo"
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-#sudo chown -R root:root /var/run/docker
-#sudo chown -R root:root /var/run/docker.sock
-sudo chown -R root:docker /var/run/docker
-sudo chown -R root:docker /var/run/docker.sock
+sudo chown -R root:root /var/run/docker
+sudo chown -R root:root /var/run/docker.sock
 
 sudo ls -al /var/run/docker /var/run/docker.sock
 
